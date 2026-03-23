f408673c4963571a90fa16af08a4c4f6934957f8
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-03-23 14:14:35 +0300

    Обновлен main, доработаны функции в db_manager.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from src.db_manager import create_database, create_tables
+from src.api import get_employer_info, get_vacancies_by_employer
+
+create_database("hh_parser")
+create_tables("hh_parser")
+
+
+employer_id = [1122462, 39305, 34692, 23427, 907345, 49357, 4509, 1740, 1603745, 1604297]
+employers_data = []
+vacancies_data = []
+
+for id in employer_id:
+    data = get_employer_info(id)
+    employers_data.append(data)
+
+for id in employer_id:
+    data = get_vacancies_by_employer(id)
+    vacancies_data.append(data['items'])
--- src/db_manager.py
+++ src/db_manager.py
@@ -11,38 +11,51 @@ def get_connection(dbname=None):
         host="localhost",
         port=5432,
         database=dbname,
-        user="postgres",
+        user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"))
 
     return conn
 
-conn = get_connection("postgres")
-conn.autocommit = True
-cur = conn.cursor()
+def create_database(dbname):
+    """Создание БД."""
+    conn = get_connection("postgres")
+    conn.autocommit = True
+    cur = conn.cursor()
 
-cur.execute("DROP DATABASE IF EXISTS hh_parser")
-cur.execute("CREATE DATABASE hh_parser")
+    cur.execute(f"DROP DATABASE IF EXISTS {dbname}")
+    cur.execute(f"CREATE DATABASE {dbname}")
 
-conn.close()
+    conn.close()
 
-conn = get_connection("hh_parser")
-conn.autocommit = True
-cur = conn.cursor()
+def create_tables(dbname):
+    """Создание таблиц."""
+    conn = get_connection(dbname)
+    conn.autocommit = True
+    cur = conn.cursor()
 
-cur.execute("CREATE TABLE employers"
-            "(employer_id INTEGER PRIMARY KEY,"
-            "name_emp VARCHAR(100),"
-            "site_url VARCHAR(100))"
-            )
-cur.execute("CREATE TABLE vacansies"
-            "(employer_id INTEGER,"
-            "name_vac VARCHAR(100),"
-            "salary_from INTEGER,"
-            "salary_to INTEGER,"
-            "salary_currency VARCHAR(10),"
-            "alternate_url TEXT)"
-            )
-cur.execute("ALTER TABLE vacansies ADD CONSTRAINT fk_vacansies_employers "
-            "FOREIGN KEY (employer_id) REFERENCES employers (employer_id)"
-            )
-conn.close()
+    cur.execute("CREATE TABLE employers"
+                "(employer_id INTEGER PRIMARY KEY,"
+                "name_emp VARCHAR(100),"
+                "site_url VARCHAR(100))"
+                )
+    cur.execute("CREATE TABLE vacancies"
+                "(employer_id INTEGER,"
+                "name_vac VARCHAR(100),"
+                "salary_from INTEGER,"
+                "salary_to INTEGER,"
+                "salary_currency VARCHAR(10),"
+                "alternate_url TEXT)"
+                )
+    cur.execute("ALTER TABLE vacancies ADD CONSTRAINT fk_vacancies_employers "
+                "FOREIGN KEY (employer_id) REFERENCES employers (employer_id)"
+                )
+    conn.close()
+
+
+# conn = get_connection("hh_parser")
+# conn.autocommit = True
+# cur = conn.cursor()
+#
+# cur.execute("INSERT INTO")
+#
+# conn.close()
