f46e2c1b0fc67aa85451a0bf966f52ea45570394
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-03-23 22:25:17 +0300

    Написаны методы для класса DBManager

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,26 +1,56 @@
+from db_utils import get_connection
+
 class DBManager:
 
-    def __init__(self):
-        pass
+    def __init__(self, dbname):
+        self.dbname = dbname
 
     def get_companies_and_vacancies_count(self):
-#       """Получает список всех компаний и количество вакансий у каждой компании"""
-        pass
+        """Получает список всех компаний и количество вакансий у каждой компании"""
+        conn = get_connection(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT employer_id, name_emp, COUNT(DISTINCT name_vac) FROM employers INNER JOIN vacancies "
+                    "ON employers.employer_id=vacancies.employer_id GROUP BY employer_id, name_emp")
+        data = cur.fetchall()
+        conn.close()
+        return data
 
     def get_all_vacancies(self):
-#       """Получает список всех вакансий с указанием названия компании,
-#       названия вакансии и зарплаты и ссылки на вакансию."""
-        pass
+        """Получает список всех вакансий с указанием названия компании,
+      названия вакансии и зарплаты и ссылки на вакансию."""
+        conn = get_connection(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT employer_id, name_emp, name_vac, salary_from|| ' - ' ||salary_to as salary, "
+                    "salary_currency, alternate_url FROM employers INNER JOIN vacancies "
+                    "ON employers.employer_id=vacancies.employer_id")
+        data = cur.fetchall()
+        conn.close()
+        return data
 
     def get_avg_salary(self):
-#       """Получает среднюю зарплату по вакансиям"""
-        pass
+        """Получает среднюю зарплату по вакансиям"""
+        conn = get_connection(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT AVG(salary_from), AVG(salary_to) FROM vacancies")
+        data = cur.fetchall()
+        conn.close()
+        return data
 
     def get_vacancies_with_higher_salary(self):
-#        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
-        pass
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        conn = get_connection(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT name_vac, salary_from FROM vacancies "
+                    "WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies)")
+        data = cur.fetchall()
+        conn.close()
+        return data
 
-    def get_vacancies_with_keyword(self):
-#        """Получает список всех вакансий,
-#        в названии которых содержатся переданные в метод слова, например python."""
-        pass
+    def get_vacancies_with_keyword(self, keyword):
+        """Получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
+        conn = get_connection(self.dbname)
+        cur = conn.cursor()
+        cur.execute("SELECT name_vac FROM vacancies WHERE name_vac LIKE %s", (f"%{keyword}%",))
+        data = cur.fetchall()
+        conn.close()
+        return data
