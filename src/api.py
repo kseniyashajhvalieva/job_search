905ed1a9294c0fcbae54dfff57e1f92fce6f0783
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-03-23 11:08:41 +0300

    Написана функция для получения информации о вакансиях

--- src/api.py
+++ src/api.py
@@ -5,3 +5,8 @@ def get_employer_info(id):
     """ Информация о работодателе."""
     response = requests.get(f"https://api.hh.ru/employers/{id}")
     return response.json()
+
+def get_vacancies_by_employer(employer_id):
+    """Информация о вакансиях."""
+    response = requests.get(f"https://api.hh.ru/vacancies?employer_id={employer_id}&per_page=10")
+    return response.json()
