f4d44bec767dd436b5e04083c10a9161fd06398a
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-03-23 15:47:40 +0300

    Обновлен main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.db_manager import create_database, create_tables, insert_employers
+from src.db_manager import create_database, create_tables, insert_employers, insert_vacancies
 from src.api import get_employer_info, get_vacancies_by_employer
 
 create_database("hh_parser")
@@ -17,4 +17,12 @@ for id in employer_id:
     data = get_vacancies_by_employer(id)
     vacancies_data.append(data['items'])
 
-insert_employers("hh_parser", employers_data)
+
+employers_tuples = [(emp["id"], emp["name"], emp["site_url"]) for emp in employers_data]
+vacancies_tuples = []
+for vac_list in vacancies_data:
+    for vac in vac_list:
+        vacancies_tuples.append((vac["employer"]["id"], vac["name"], vac["salary"]["from"] if vac["salary"] else None, vac["salary"]["to"] if vac["salary"] else None, vac["salary"]["currency"] if vac["salary"] else None, vac["alternate_url"]))
+
+insert_employers("hh_parser", employers_tuples)
+insert_vacancies("hh_parser", vacancies_tuples)
